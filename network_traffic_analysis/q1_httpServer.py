c7e23350a2a7d853d845e3c1d7a75c7e2a9f8736
hrishi-deshpande <hris.desh@gmail.com>
2018-02-21 16:24:37 -0500

    HTTP servers

---
+++ network_traffic_analysis/q1_httpServer.py
@@ -0,0 +1,16 @@
+import pyshark
+
+#First read the input trace file. We need to obtain those packets that have http as the application protocol
+#and for whom the http response was properly returned back to the client. 
+input_trace = pyshark.FileCapture("project2.trace", display_filter='http and http.response.code != 0')
+
+#Create a set of IP addresses of the http servers involved in a valid HTTP exchange.
+#A set can only have unique IP addresses.
+http_server_set = set()
+
+#First input the file. 
+for packet in input_trace:
+	http_server_set.add(packet.ip.src)
+
+#Next print out the set contents
+print(http_server_set)
